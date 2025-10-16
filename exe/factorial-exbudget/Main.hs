c6d61de109163d4c9829c788831418a58f7fb5b4
Seungheon Oh <me@sh5.org>
2025-10-16 21:59:58 +0900

    Add exbudget and size variants of each submissions

--- exe/factorial-exbudget/Main.hs
+++ exe/factorial-exbudget/Main.hs
@@ -5,14 +5,15 @@ import Plutarch.Internal.Term (punsafeBuiltin, punsafeCoerce)
 import Plutarch.Prelude
 import PlutusCore qualified as PLC
 
--- conditional with no hoisting
+-- conditional without hoisting the IfThenElse builtin.
 pif'' :: Term s PBool -> Term s a -> Term s a -> Term s a
 pif'' cond ifT ifF =
   pforce $
     pforce (punsafeBuiltin PLC.IfThenElse) # cond # pdelay ifT # pdelay ifF
 
+-- pfix but inlines the recursion point twice.
 pfix' :: (Term s (a :--> b) -> Term s (a :--> b)) -> Term s (a :--> b)
-pfix' f = plam (\r -> punsafeCoerce r # r) # plam (\r -> f (punsafeCoerce r # r))
+pfix' f = plam (\r -> f (punsafeCoerce r # r)) # plam (\r -> f (punsafeCoerce r # r))
 
 pfactorial :: Term s (PInteger :--> PInteger)
 pfactorial =
@@ -20,8 +21,5 @@ pfactorial =
   pfix' $ \r -> plam $ \x ->
     pif'' (x #<= 0) 1 (x * (r # (x - 1)))
 
--- This bloats the script size, but faster
--- punrollBound 11 perror $ \r -> plam $ \x -> pif (x #== 1) x (x * (r # (x - 1)))
-
 main :: IO ()
-main = compileToUplc pfactorial "factorial.uplc"
+main = compileToUplc pfactorial "factorial_exbudget.uplc"
